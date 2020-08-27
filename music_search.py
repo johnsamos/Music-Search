85c8f73de9fb71c73e0c6d4c1267fe780285f179
John Bourboulas <46540325+johnsamos@users.noreply.github.com>
2020-08-28 01:48:36 +0300

    Update music_search.py

--- music_search.py
+++ music_search.py
@@ -1,14 +1,13 @@
-# in this project you can play music from the path you will give
 from pygame import mixer, time
 import os
         
 print("what would you like to listen")
-mypath='/home/john/Desktop/Assistant'   # Here you can change the path of the music
-inp=input("give folder: ")  # Here you say to the computer wich folder you would like to play 
+mypath='/Users/johny/Music/'   # Here you can change the path of the music
+inp=input("give folder: ")  # input your folder name for example rock to start playing music from this file 
 mixer.init()
 mypath=os.path.join(mypath,inp)
 for song in os.listdir(mypath):
-    if song.endswith(".mp3"):    # For now the program open all the files who ends with .mp3
+    if song.endswith(".mp3"):   
         print('Playing:',song)
         mixer.music.load(os.path.join(mypath,song))
         mixer.music.play()
